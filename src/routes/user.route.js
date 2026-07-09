579d18e463a4d43867f6e8267dbc11a9ddb6806b
Opeyemi Ogunshina <ogunshinaopeyemi07@gmail.com>
2026-07-09 12:15:56 +0100

    Added additional layer of protection

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { registerUser, loginUser, getUser } = require("../controllers/user.controller.js");
+const requireAuth = require("../middlewares/requireAuth.js");
 
 const route = express.Router();
 
 route.post("/sign-up", registerUser);
 route.post("/log-in", loginUser);
-route.get("/users", getUser)
+route.get("/users", requireAuth, getUser)
 
 module.exports = route;
